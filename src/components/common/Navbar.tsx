3cd18a0e2e14403096df30965e4d6b2f211c6f0e
Shaik Yaseen <hackercanhack69@gmail.com>
2024-11-14 22:44:21 +0530

    Made the folder structure

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = async(props: Props) => {
+  return (
+    <div className='w-full h-[3rem] border-none bg-black'>
+
+    </div>
+  )
+}
+
+export default Navbar
