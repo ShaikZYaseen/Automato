5f9da9a9bccc3be20d3cf596a9f24d846b852309
Shaik Yaseen <hackercanhack69@gmail.com>
2024-11-16 20:03:21 +0530

    added files

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BackgroundBeams } from './ui/BackgroundBeams'
+import tomato from "../../../public/tomato.png"
+import Image from 'next/image'
 
 type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <div className='text-white'>
+    <div className='text-white h-full w-screen flex-col  items-center justify-between'>
+      <div><p>Automate your workflow</p>
+      <div className='flex'>with <Image src={tomato} className='h-10 w-10 ml-2 rounded-2xl' alt='tomato'></Image></div>
+      </div>
     <BackgroundBeams/>
     </div>
   )
